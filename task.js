5f74cb39b8e79d2169280973e5ad153e38f15667
mosius27 <mosius27@yandex.ru>
2023-08-16 20:27:55 +0300

    .

---
+++ task.js
@@ -0,0 +1,45 @@
+// Функция, которая возвращает элемент лунки по индексу
+function getHole(index) {
+    return document.getElementById(`hole${index}`);
+  }
+  
+  // Обработчик клика на лунку
+  function handleHoleClick(event) {
+    const hole = event.currentTarget;
+  
+    if (hole.classList.contains('hole_has-mole')) {
+      // Если в лунке есть крот, увеличиваем счетчик убитых кротов
+      const deadCounter = document.getElementById('dead');
+      deadCounter.textContent = +deadCounter.textContent + 1;
+  
+      // Убираем класс hole_has-mole, чтобы убрать крота
+      hole.classList.remove('hole_has-mole');
+    } else {
+      // Если в лунке нет крота, увеличиваем счетчик промахов
+      const lostCounter = document.getElementById('lost');
+      lostCounter.textContent = +lostCounter.textContent + 1;
+    }
+  
+    // Проверяем условия победы и поражения
+    if (+deadCounter.textContent === 10) {
+      alert('Поздравляем! Вы победили!');
+      resetCounters();
+    } else if (+lostCounter.textContent === 5) {
+      alert('Игра окончена. Вы проиграли.');
+      resetCounters();
+    }
+  }
+  
+  // Функция для сброса счетчиков
+  function resetCounters() {
+    const deadCounter = document.getElementById('dead');
+    const lostCounter = document.getElementById('lost');
+    deadCounter.textContent = '0';
+    lostCounter.textContent = '0';
+  }
+  
+  // Регистрируем обработчик для каждой лунки
+  for (let i = 1; i <= 9; i++) {
+    const hole = getHole(i);
+    hole.addEventListener('click', handleHoleClick);
+  }
